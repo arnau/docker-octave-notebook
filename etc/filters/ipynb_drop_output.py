9344deafef7baa27887f65f27cf7822d9529ff9c
Arnau Siches <asiches@gmail.com>
2016-07-25 07:28:55 +0100

    refactor(git): Force UTF-8 when loading/dumping json in ipynb_drop filter

--- etc/filters/ipynb_drop_output.py
+++ etc/filters/ipynb_drop_output.py
@@ -11,7 +11,7 @@ import sys
 import json
 
 nb = sys.stdin.read()
-json_in = json.loads(nb)
+json_in = json.loads(nb, encoding="utf-8")
 nb_metadata = json_in["metadata"]
 
 def strip_output_from_cell(cell):
@@ -24,4 +24,10 @@ def strip_output_from_cell(cell):
 for cell in json_in["cells"]:
     strip_output_from_cell(cell)
 
-json.dump(json_in, sys.stdout, sort_keys=True, indent=1, separators=(",",": "), ensure_ascii=False)
+json.dump(json_in,
+          sys.stdout,
+          sort_keys=True,
+          indent=1,
+          separators=(",",": "),
+          ensure_ascii=False,
+          encoding="utf-8")
